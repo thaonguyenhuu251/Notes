070b455db75aa14cc3a51b9932ed7fa3ca1072fe
thaonguyenhuu251 <htnguyendev@gmail.com>
2022-10-16 22:05:43 +0700

    Update base 5

---
+++ app/src/main/java/com/example/notes/Constants.kt
@@ -0,0 +1,16 @@
+package com.example.notes
+
+object Constants {
+    const val ID_WORK = "idwork"
+    const val NAME_WORK = "namework"
+    const val CONTENT_WORK = "contentwork"
+    const val START_DAY = "startday"
+    const val TIME_COMPLETE = "timecomplete"
+    const val ALL = "all"
+    const val DATE_IN = "datein"
+    const val DATE_OUT = "dateout"
+    const val TIME_IN = "timein"
+    const val TIME_OUT = "timeout"
+    const val NAME_IN = "namein"
+    const val NAME_OUT = "nameout"
+}
