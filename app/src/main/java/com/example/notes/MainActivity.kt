9446bf45628bfd69a5e6f0bc856e5f85d755ae2f
nguyenhuuthao251 <htnguyendev@gmail.com>
2022-10-19 07:04:31 +0700

    Update mainactivity

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.notes
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
