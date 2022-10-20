71ab4ed50da673039e230a4647405a8a1ed8831a
Gamcute <honggam26052001@gmail.com>
2022-10-20 11:24:41 +0700

    update layout trash setting, dialog delete, create layout fragment markbook  by nguyen gam

--- app/src/main/java/com/example/notes/ChartFragment.kt
+++ app/src/main/java/com/example/notes/ChartFragment.kt
@@ -34,7 +34,7 @@ class ChartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false)
+        return inflater.inflate(R.layout.fragment_markbook, container, false)
     }
 
     companion object {
--- app/src/main/java/com/example/notes/TrashActivity.kt
+++ app/src/main/java/com/example/notes/TrashActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class TrashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_trash)
+        setContentView(R.layout.trash_setting)
     }
 }
