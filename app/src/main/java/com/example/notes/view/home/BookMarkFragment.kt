6b40f7620c4bedaa695f8c65bee231c7ed0da385
Gamcute <honggam26052001@gmail.com>
2022-10-23 17:55:12 +0700

    creat layout bookmark by Nguyen Gam

--- app/src/main/java/com/example/notes/view/home/BookMarkFragment.kt
+++ app/src/main/java/com/example/notes/view/home/BookMarkFragment.kt
@@ -5,37 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.notes.R
 import com.example.notes.adapter.HomePagerFragmentAdapter
-import com.example.notes.databinding.FragmentChartBinding
-import com.example.notes.databinding.FragmentHomeBinding
-import com.github.mikephil.charting.utils.Utils.init
+import com.example.notes.databinding.FragmentBookMarkBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ChartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ChartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+class BookMarkFragment : Fragment() {
     private val titles = arrayOf("Note", "Work")
-    private lateinit var binding:FragmentChartBinding
+    private lateinit var binding:FragmentBookMarkBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -43,7 +26,7 @@ class ChartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentChartBinding.inflate(layoutInflater)
+        binding = FragmentBookMarkBinding.inflate(layoutInflater)
         return binding.root
     }
 
@@ -78,23 +61,4 @@ class ChartFragment : Fragment() {
         })
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChartFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
