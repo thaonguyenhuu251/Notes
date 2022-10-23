3d5ddffbd71d287eec1b5c948ae0c874b1f3ddc2
thaonguyenhuu251 <htnguyendev@gmail.com>
2022-10-23 09:28:27 +0700

    Update dialog

--- app/src/main/java/com/example/notes/view/components/DateDialog.kt
+++ app/src/main/java/com/example/notes/view/components/DateDialog.kt
@@ -1,6 +1,46 @@
 package com.example.notes.view.components
 
+import android.app.Dialog
+import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.DialogFragment
+import com.example.notes.R
+import com.example.notes.databinding.DialogDayBinding
 
 class DateDialog : DialogFragment() {
+    private lateinit var binding: DialogDayBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DialogDayBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val dialog = Dialog(requireContext())
+        val window = dialog.window
+        window!!.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        setStyle(STYLE_NO_TITLE, R.style.MyDialog)
+        val windowAttributes: WindowManager.LayoutParams = window.attributes!!
+        windowAttributes.gravity = Gravity.CENTER_VERTICAL
+        window.attributes = windowAttributes
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    companion object {
+        const val TAG = "DateDialog"
+    }
 }
---
+++ app/src/main/java/com/example/notes/view/home/AddWorkActivity.kt
@@ -0,0 +1,160 @@
+package com.example.notes.view.home
+
+import com.example.notes.util.FileUtils.hideKeyboard
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TimePicker
+import android.widget.DatePicker
+import android.widget.TextView
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.notes.databinding.ActivityAddWorkBinding
+import com.example.notes.view.components.DateDialog
+import java.lang.Exception
+
+class AddWorkActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddWorkBinding
+    var timepicker: TimePicker? = null
+    private val datePicker: DatePicker? = null
+    var hour = 0
+    var minutes = 0f
+    var txtDone: TextView? = null
+    var txtExit: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddWorkBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+        binding.root.setOnClickListener { v: View? -> hideKeyboard(this) }
+        binding.txtBack.setOnClickListener { v: View? -> onBackPressed() }
+        binding.edtTimeComplete.setOnClickListener { v: View? ->
+
+        }
+        binding.edtStartDay.setOnClickListener { v: View? ->
+            val dialogDate = DateDialog()
+            dialogDate.show(supportFragmentManager, dialogDate.tag)
+        }
+        binding.btnAddWork.setOnClickListener { v: View? ->
+            val timeComplete: Float
+            val nameWork: String
+            val startDay: String
+            val contentWork: String
+            try {
+                timeComplete = hour + (minutes / 60)
+                nameWork = binding.edtNameWork.text.toString().trim { it <= ' ' }
+                startDay = binding.edtStartDay.text.toString().trim { it <= ' ' }
+                contentWork = binding.edtContentWork.text.toString().trim { it <= ' ' }
+                if (timeComplete < 0 || timeComplete > 8) {
+                    Toast.makeText(this, "time >0 or time < 8", Toast.LENGTH_SHORT).show()
+                    binding.edtTimeComplete.setText("")
+                } else {
+                    /*Work work = new Work();
+                    work.setIdWork(System.currentTimeMillis());
+                    work.setNameWork(nameWork);
+                    work.setStartDay(startDay);
+                    work.setContentWork(contentWork);
+                    work.setTimeComplete(timeComplete);
+                    MainActivity.roomDatabaseClass.workDao().addWork(work);*/
+                    Toast.makeText(this, "Data Successfully saved", Toast.LENGTH_SHORT).show()
+                    binding.edtStartDay.setText("")
+                    binding.edtNameWork.setText("")
+                    binding.edtTimeComplete.setText("")
+                    binding.edtContentWork.setText("")
+                    onBackPressed()
+                }
+            } catch (e: Exception) {
+                Toast.makeText(this, "do not leave blank", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    } /*private void openDialogTime(int gravity) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_time);
+
+        Window window = dialog.getWindow();
+        if (window == null) {
+            return;
+        }
+
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+        windowAttributes.gravity = gravity;
+        window.setAttributes(windowAttributes);
+
+        if (Gravity.CENTER == gravity) {
+            dialog.setCancelable(true);
+        } else {
+            dialog.setCancelable(false);
+        }
+        txtDone = dialog.findViewById(R.id.txtDone);
+        txtExit = dialog.findViewById(R.id.txtExit);
+
+        txtDone.setOnClickListener(v->{
+            hour = timepicker.getCurrentHour();
+            minutes = timepicker.getCurrentMinute();
+            binding.edtTimeComplete.setText(FileUtils.formatTimeNew(hour, (int) minutes));
+            dialog.dismiss();
+        });
+
+        txtExit.setOnClickListener(v->{
+            dialog.dismiss();
+        });
+        timepicker = dialog.findViewById(R.id.timePicker);
+        timepicker.setIs24HourView(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            timepicker.setHour(01);
+            timepicker.setMinute(00);
+        }
+
+        dialog.show();
+    }*/
+    /*@SuppressLint("DefaultLocale")
+    private void openDialogDay(int gravity) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_day);
+        datePicker = dialog.findViewById(R.id.dayPicker);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+
+        Window window = dialog.getWindow();
+        if (window == null) {
+            return;
+        }
+
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+        windowAttributes.gravity = gravity;
+        window.setAttributes(windowAttributes);
+
+        if (Gravity.CENTER == gravity) {
+            dialog.setCancelable(true);
+        } else {
+            dialog.setCancelable(false);
+        }
+        txtDone = dialog.findViewById(R.id.txtDone);
+        txtExit = dialog.findViewById(R.id.txtExit);
+
+        txtDone.setOnClickListener(v->{
+            int day = this.datePicker.getDayOfMonth();
+            int year = this.datePicker.getYear();
+            int month = this.datePicker.getMonth() + 1;
+            binding.edtStartDay.setText(FileUtils.formatCalendar(day, month, year));
+            dialog.dismiss();
+        });
+
+        txtExit.setOnClickListener(v->{
+            dialog.dismiss();
+        });
+
+        dialog.show();
+    }*/
+}
